65ca77d13c1c8fce200965b87e61ae6185616339
icarusw-code <77170611+icarusw-code@users.noreply.github.com>
2021-10-16 21:26:24 +0900

    list

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,8 @@ PROJECT_APPS = [
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
     "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationsConfig",
+    "lists.apps.ListsConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
